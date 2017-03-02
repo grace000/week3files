a08d89295c4ae6c79e75156d1f2e103d7c73d7eb
grace000 <tiffani.grace.elmore@gmail.com>
2017-03-02 02:00:05 -0600

    grouphelp changes

--- assets/javascript.js/game.js
+++ assets/javascript.js/game.js
@@ -1,9 +1,6 @@
 
-
-  $(document).ready(function) {
-
   // This array holds the words that the player will choose from.
-  var words = [
+  var wordBank = [
     'chicago', 
     'rush', 
     'lawrence', 
@@ -26,55 +23,63 @@
     'lincoln',
 
     ];
-  
-  var guess ;                       // Guess
-  var previousGuesses = [ ];        // Stored geusses
-  var guessesLeft ;                 // Guesses left
-  var wins ;                        // Count correct guesses
-  var wrongAnswerCount = [];        // Stored losses
 
-   // This is how a word will be randomly chosen from the list.
-  var randomWord = words[Math.floor(Math.random() * words.length)];
 
-    // Empty array to store guesses
-    var answerArray = [];
+  //Now define global variables 
+  var wins = 0;                     // sets the initial value of the wins, to be manipulated later as the user wins
+  var losses = 0;
+  var lives = 7;                    // 7 lives to start with
+  var guessesRemaining = 7;         // Guesses left
+  var wrongGuess = [];              
+  var selectWord = null;            //run a selctor to fill that in with current word
+  var answerArray = [];             // Empty array to store guesses
+
+
+   // This is how a word will be randomly chosen from the word bank.
+  var updateSelectWord = function () {
+    selectWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    for (var i = 0; i < selectWord.length; i++) {
+        //adding blank tiles to the screen. Each tile should hold a letter.
+         answerArray[i] + "_";
+          console.log("test");
+  };
+};
+
+  //This function will take the script and place into HTML an update of guesses remaining after each guess.
+  var updateGuessesRemaining = function() {
+    document.querySelector('#guessesRemaining').innerHTML = "Lives left: " + guessesRemaining;
+  };
+
   
+  //Setting up how the guesses will join on HTML
+  var updatewrongGuess = function() {
+    document.querySelector('#wrongGuess').innerHTML = "Your Guesses so far: " + wrongGuess.join(', ');
+  };
+
+
+
   //Game Screen
-  function gameScreen(){
-    $('#game-container').empty(); 
-    $('#gameContent').append('<div id="wordHolder"></div>');
-    $('#gameContent').append('<div id="guesses">Previous guesses:</div>');
-    $('#gameContent').append('<div id="feedback"></div>');
 
-    getWord(); 
-    var numberOfTiles=randomWord.length; 
-    wrongAnswerCount=0; 
-    previousGuesses=[];
 
-    for(i=0;i<numberOfTiles;i++){ 
-    $('#wordHolder').append('<div class="tile" id=t'+i+'></div>');
-}
+   
+//resets page once we invoke it - we won't invoke it until we either win or lose the game
+var reset = function() {
+    lives = 7;
+    guessesRemaining = 7;
+    wrongGuess = [];
+    updateSelectWord();
+    updateGuessesRemaining();
+    updatewrongGuess();
+};
 
-  $(document).on("keyup",handleKeyUp);
+updateSelectWord();
+updateGuessesRemaining();
 
-  //filling the answer array with under scores.
-  lineSet = function () {
-    
-    randomWord ();
-
-    for (var i = 0; i < randomWord.lenth; i++)
-    {
-        answerArray[i] = "_";
-    }
-
-    var remainingLetters = letter.length;
 
     
        
 
-    // The end of the game loop
-
-    };
+ 
 
     
 
